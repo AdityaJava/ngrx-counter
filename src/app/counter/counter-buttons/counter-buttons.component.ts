3b5093ce33ddca4c0c1f884cc6c33f6cef755c74
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-10 12:27:02 +0530

    feat: Enhance counter buttons with event emitters and update template bindings

--- src/app/counter/counter-buttons/counter-buttons.component.ts
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter-buttons',
@@ -6,6 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './counter-buttons.component.html',
   styleUrl: './counter-buttons.component.css'
 })
-export class CounterButtonsComponent {
+export class CounterButtonsComponent implements OnInit{
+  ngOnInit(): void {
+    // Initialization logic can go here if needed
+  }
+
+  @Output()
+  incrementEvent : EventEmitter<void> = new EventEmitter();
+
+  @Output()
+  decrementEvent : EventEmitter<void> = new EventEmitter();
+  
+  @Output()
+  resetEvent : EventEmitter<void> = new EventEmitter();
+
+  onIncrement() {
+    this.incrementEvent.emit();
+  }
+
+  onDecrement() {
+    this.decrementEvent.emit();
+  }
+
+  onReset() {
+    this.resetEvent.emit();
+  }
 
 }
